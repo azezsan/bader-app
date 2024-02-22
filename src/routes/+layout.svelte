e816631ccbb6eea11039812dacc56258622d51b6
azez <t.s.connect.m@gmail.com>
2024-02-22 03:13:15 +0300

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.pcss';
+	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
+	import { i18n } from '$lib/i18n.js';
+</script>
+
+<ParaglideJS {i18n}>
+	<slot />
+</ParaglideJS>
