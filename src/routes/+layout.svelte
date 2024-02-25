728e457eaacbbb9ac3c232313c9f6e3e04f529e1
azez <t.s.connect.m@gmail.com>
2024-02-25 13:11:16 +0300

    undo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,11 @@
 	import { ParaglideJS } from '@inlang/paraglide-js-adapter-sveltekit';
 	import { i18n } from '$lib/i18n.js';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
 <ParaglideJS {i18n}>
 	<Toaster />
+	<ModeWatcher defaultMode="light" />
 	<slot />
 </ParaglideJS>
